70d5c24c52a33d02b5d084186265ccd926a6199a
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-08-06 23:51:23 +0900

    いいねリンク追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    like = current_user.likes.build(song_id: params[:song_id])
+    if like.save
+      redirect_to songs_path, notice: "曲をいいねしました！"
+    else
+      redirect_to songs_path, alert: "いいねに失敗しました。"
+    end
+  end
 end
