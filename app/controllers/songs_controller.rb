e526b96a7fd8a3582cd1c6aa9a49b11b72f0a98d
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-08-11 00:33:41 +0900

    add pagination

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
 
   def index
-    @songs = Song.all
+    @songs = Song.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def create
@@ -21,12 +21,35 @@ class SongsController < ApplicationController
     redirect_to root_path, status: :see_other, notice: "曲が正常に削除されました!"
   end
 
-  def search
-    keyword = params[:keyword]
-    @results = YoutubeSearchService.new(keyword).search
+  # def search
+  #   keyword = params[:keyword]
+  #   @results = YoutubeSearchService.new(keyword).search
 
-    render :new
-  end
+  #   render :new
+  # end
+
+  # def search
+  #   keyword = params[:keyword]
+  #   page_token = params[:page_token] # 次のページトークン
+  #   results = YoutubeSearchService.new(keyword).search
+  #   @results = Kaminari.paginate_array(results).page(params[:page]).per(10)
+
+  #   # 次のページトークンをビューで使用できるようにします
+  #   @next_page_token = next_page_token
+
+  #   render :new
+  # end
+
+  def search
+  keyword = params[:keyword]
+  page_token = params[:page_token]
+  @results, @next_page_token = YoutubeSearchService.new(keyword, page_token).search
+
+  render :new
+end
+
+
+
 
 
   private
--- app/services/youtube_search_service.rb
+++ app/services/youtube_search_service.rb
@@ -2,30 +2,40 @@ class YoutubeSearchService
   require 'net/http'
   require 'json'
 
-  def initialize(keyword)
+  # def initialize(keyword)
+  #   @keyword = keyword
+  #   @api_key = ENV['YOUTUBE_API_KEY']
+  # end
+
+  def initialize(keyword, page_token = nil)
     @keyword = keyword
+    @page_token = page_token
     @api_key = ENV['YOUTUBE_API_KEY']
   end
 
-  def search
+
+  def search(page_token = nil)
     base_url = 'https://www.googleapis.com/youtube/v3/search'
     params = {
       key: @api_key,
       q: @keyword,
       type: 'video',
-      maxResults: 10, # 10件の検索結果を取得
-      part: 'snippet'
+      maxResults: 20, # 10件の検索結果を取得
+      part: 'snippet',
+      pageToken: page_token # 次のページトークン
     }
     uri = URI(base_url)
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get_response(uri)
     json = JSON.parse(response.body)
-    json['items'].map do |item|
+    results = json['items'].map do |item|
       {
         title: item['snippet']['title'],
         artist: item['snippet']['channelTitle'],
         youtube_url: "https://www.youtube.com/watch?v=#{item['id']['videoId']}"
       }
     end
-  end
+    next_page_token = json['nextPageToken']
+    [Kaminari.paginate_array(results).page(params[:page]).per(20), next_page_token]
+end
 end
